e86342f3db65453dd8f5fb9c3a363e84ea1a9216
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2018-09-07 11:15:51 -0400

    add test excpecption handling through abstract logger

--- src/test/java/todd/spike/AbstractCustomJsonLayoutExceptionTest.java
+++ src/test/java/todd/spike/AbstractCustomJsonLayoutExceptionTest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.exc.InvalidDefinitionException;
+import org.apache.logging.log4j.core.LogEvent;
 import org.apache.logging.log4j.core.impl.MutableLogEvent;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,6 +13,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,10 +21,10 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-public class MyCustomJsonLayoutExceptionTest {
+public class AbstractCustomJsonLayoutExceptionTest {
 
     @InjectMocks
-    MyCustomJsonLayout jsonLayout;
+    ConcreteCustomJsonLayout jsonLayout;
 
     @Mock
     ObjectMapper objectMapper;
@@ -37,4 +39,26 @@ public class MyCustomJsonLayoutExceptionTest {
         assertThat(jsonLayout.toSerializable(event)).isEqualTo("com.fasterxml.jackson.databind.JsonMappingException: N/A");
     }
 
+}
+
+class ConcreteCustomJsonLayout extends AbstractCustomJsonLayout {
+
+    public ConcreteCustomJsonLayout() {
+        super(false);
+    }
+
+    @Override
+    protected Map<String, Object> addCustomPairsToKvMap(LogEvent event) {
+        return new HashMap<>();
+    }
+
+    @Override
+    protected Map<String, Object> logExceptionRecursively(Throwable throwable) {
+        return new HashMap<>();
+    }
+
+    @Override
+    protected Map<String, Object> logStackTrace(Throwable throwable) {
+        return new HashMap<>();
+    }
 }
